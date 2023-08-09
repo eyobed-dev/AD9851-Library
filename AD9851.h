ade7629a179b55cf22433b42ec9cff4b9048b227
eyobed-dev <103148982+eyobed-dev@users.noreply.github.com>
2023-08-09 14:39:48 +0300

    Update AD9851.h

--- AD9851.h
+++ AD9851.h
@@ -1,35 +1,32 @@
-#ifndef AD9850_H
-#define AD9850_H
+#ifndef AD9851_H
+#define AD9851_H
 
 #include <Arduino.h>
 
 #define pulseHigh(pin) digitalWrite(pin, HIGH); delayMicroseconds(10); digitalWrite(pin, LOW)
+#define pow2_32 4294967296
 
-class AD9850
-{
-  public:
+class AD9851 {
+public:
+    void InitSerial(int W_CLK, int FQ_UD, int RESET, int DATA);
+    void InitParallel(int W_CLK, int FQ_UD, int RESET, volatile uint8_t* port);
+    void SetFrequency(unsigned long frequency);
+    void SetPhase(int phase);
+    void Load();
+    void Reset();
 
-
-    void initSerial(int , int , int , int );
-    void initParallel(int , int , int , volatile uint8_t *);
-    void setFrequency(unsigned long );
-    void setPhase(int );
-    void load();
-    void reset();
-    
-    
-  private:
-      int _W_CLK;
-      int _FQ_UD;
-      int _RESET;
-      int _DATA;
-      volatile uint8_t _port=0x0;
-      bool serialMode;
-      unsigned long frequencyWord = 0x0;
-      int phaseWord = 0;
-      const long DDS_CLOCK = 180000000;
-      void loadSerial();
-      void loadParallel();
-  };
+private:
+    int _W_CLK;
+    int _FQ_UD;
+    int _RESET;
+    int _DATA;
+    volatile uint8_t _port = 0x0;
+    bool serialMode;
+    unsigned long frequencyWord = 0x0;
+    int phaseWord = 0;
+    const long DDS_CLOCK = 180000000;
+    void LoadSerial();
+    void LoadParallel();
+};
 
 #endif

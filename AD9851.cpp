3b47c40d85dd4d0490523a4d535d81dfd7f047ea
eyobed-dev <103148982+eyobed-dev@users.noreply.github.com>
2023-08-09 14:39:28 +0300

    Update AD9851.cpp

--- AD9851.cpp
+++ AD9851.cpp
@@ -1,11 +1,8 @@
 #include <Arduino.h>
 #include "AD9851.h"
 
-
-
-
-void AD9850::initSerial(int W_CLK, int FQ_UD, int RESET, int DATA) {
-  //Serial init
+void AD9851::InitSerial(int W_CLK, int FQ_UD, int RESET, int DATA) {
+  // Serial init
   serialMode = true;
 
   _W_CLK = W_CLK;
@@ -18,28 +15,26 @@ void AD9850::initSerial(int W_CLK, int FQ_UD, int RESET, int DATA) {
   pinMode(_DATA, OUTPUT);
   pinMode(_RESET, OUTPUT);
 
-  // pulldown initializations
+  // Pulldown initializations
   digitalWrite(_DATA, LOW);
   digitalWrite(_FQ_UD, LOW);
   digitalWrite(_W_CLK, LOW);
 
   pulseHigh(_RESET);
 
-  // if you are not using the parallel port, tie pins D0 and D1 to 5V via a resistor and D2 to GND
+  // If you are not using the parallel port, tie pins D0 and D1 to 5V via a resistor and D2 to GND
   _port = B11000011;
   pulseHigh(_W_CLK);
   pulseHigh(_FQ_UD);
   _port = B00000000;
-  setPhase(0);
-  setFrequency(0);
-  load();
+  SetPhase(0);
+  SetFrequency(0);
+  Load();
 }
 
-void AD9850::initParallel(int W_CLK, int FQ_UD, int RESET, volatile uint8_t *port) {
-  //Parallel init
+void AD9851::InitParallel(int W_CLK, int FQ_UD, int RESET, volatile uint8_t *port) {
+  // Parallel init
   serialMode = false;
-
-
 
   _W_CLK = W_CLK;
   _FQ_UD = FQ_UD;
@@ -50,9 +45,6 @@ void AD9850::initParallel(int W_CLK, int FQ_UD, int RESET, volatile uint8_t *por
   pinMode(_FQ_UD, OUTPUT);
   pinMode(_RESET, OUTPUT);
 
-
-
-  DDRD = B11111111;
 
   // Determine the DDRx register based on the port
   volatile uint8_t *ddrRegister;
@@ -69,22 +61,18 @@ void AD9850::initParallel(int W_CLK, int FQ_UD, int RESET, volatile uint8_t *por
   }
 
   // Set the entire port as output using the determined DDRx register
-
   // *ddrRegister = 0xFF; // Set DDRx register value
 
   pulseHigh(_RESET);
-
 }
 
-void AD9850::setFrequency(unsigned long frequency) {
-
-  frequencyWord = (frequency * pow(2, 32) * 1UL) / DDS_CLOCK;
+void AD9851::SetFrequency(unsigned long frequency) {
+  frequencyWord = (frequency * pow2_32 * 1UL) / DDS_CLOCK;
 }
 
-void AD9850::setPhase(int phase) {
-
+void AD9851::SetPhase(int phase) {
   // Normalize the phase angle between 0 and 359.999 degrees
-  int  desiredPhase = fmod(desiredPhase, 360);
+  int desiredPhase = fmod(desiredPhase, 360);
   if (desiredPhase < 0)
     desiredPhase += 360;
 
@@ -92,28 +80,22 @@ void AD9850::setPhase(int phase) {
   uint8_t phaseWord = static_cast<uint8_t>((desiredPhase / 360.0) * 16);
 }
 
-void AD9850::load() {
-  if (serialMode)
-  {
-    AD9850::loadSerial();
-  }
-  else
-  {
-    AD9850::loadParallel();
+void AD9851::Load() {
+  if (serialMode) {
+    AD9851::LoadSerial();
+  } else {
+    AD9851::LoadParallel();
   }
 }
 
-void AD9850::loadSerial() {
-
-  for (int i = 0; i < 32; i++)
-  {
+void AD9851::LoadSerial() {
+  for (int i = 0; i < 32; i++) {
     digitalWrite(_DATA, (frequencyWord & 0x01));
     pulseHigh(_W_CLK);
     frequencyWord >>= 1;
   }
 
-  for (int i = 0; i < 4; i++)
-  {
+  for (int i = 0; i < 4; i++) {
     if (i < 1)
       digitalWrite(_DATA, HIGH);
     else
@@ -122,9 +104,8 @@ void AD9850::loadSerial() {
     pulseHigh(_W_CLK);
   }
 
-  for (int i = 0; i < 4; i++)
-  {
-    // send phase
+  for (int i = 0; i < 4; i++) {
+    // Send phase
     digitalWrite(_DATA, (phaseWord & 0x01));
     pulseHigh(_W_CLK);
     phaseWord >>= 1;
@@ -133,34 +114,34 @@ void AD9850::loadSerial() {
   pulseHigh(_FQ_UD);
 }
 
-void AD9850::loadParallel()
-{
-  // pulse fu_ud
+void AD9851::LoadParallel() {
+  // Pulse FQ_UD
   pulseHigh(_FQ_UD);
 
-  // write w0
+  // Write W0
   _port = B00000001;
   pulseHigh(_W_CLK);
 
-  // write w1
+  // Write W1
   _port = (byte)(frequencyWord >> 24);
   pulseHigh(_W_CLK);
 
-  // write w2
+  // Write W2
   _port = (byte)(frequencyWord >> 16);
   pulseHigh(_W_CLK);
 
-  // write w3
+  // Write W3
   _port = (byte)(frequencyWord >> 8);
   pulseHigh(_W_CLK);
 
-  // write w4
+  // Write W4
   _port = (byte)frequencyWord;
   pulseHigh(_W_CLK);
-  // update frequency
+
+  // Update frequency
   pulseHigh(_FQ_UD);
 }
 
-void AD9850::reset() {
- pulseHigh(_RESET);
+void AD9851::Reset() {
+  pulseHigh(_RESET);
 }

c64e3e06182f3715eb41bc3939a0b64a0caa5b2f
eyobed-dev <103148982+eyobed-dev@users.noreply.github.com>
2023-09-15 15:25:17 +0300

    Update AD9851.h

--- AD9851.h
+++ AD9851.h
@@ -1,17 +1,28 @@
+/*
+  Author: Eyobed A.
+  Institiution: INSA, 2016
+  License: GPL v3.0+
+  Brief: header files
+*/
+
+
 #ifndef AD9851_H
 #define AD9851_H
 
 #include <Arduino.h>
 
-#define pulseHigh(pin) digitalWrite(pin, HIGH); delayMicroseconds(10); digitalWrite(pin, LOW)
-#define pow2_32 4294967296
+#define pulseHigh(pin) digitalWrite(pin, HIGH); delayMicroseconds(1); digitalWrite(pin, LOW)
+
 
 class AD9851 {
+  
 public:
     void InitSerial(int W_CLK, int FQ_UD, int RESET, int DATA);
-    void InitParallel(int W_CLK, int FQ_UD, int RESET, volatile uint8_t* port);
+    void InitParallel(int W_CLK, int FQ_UD, int RESET, volatile byte *port);
     void SetFrequency(unsigned long frequency);
     void SetPhase(int phase);
+    void EnablePLL(bool pll);
+    void powerDown(bool power);
     void Load();
     void Reset();
 
@@ -20,10 +31,13 @@ private:
     int _FQ_UD;
     int _RESET;
     int _DATA;
-    volatile uint8_t _port = 0x0;
-    bool serialMode;
+    volatile byte *_port;
+    bool serialMode = false;
+    bool pllState = true;
+    bool powerOn = true;
     unsigned long frequencyWord = 0x0;
     int phaseWord = 0;
+    const uint64_t pow2_32 = 4294967296;
     const long DDS_CLOCK = 180000000;
     void LoadSerial();
     void LoadParallel();
